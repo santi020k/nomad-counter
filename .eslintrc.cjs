5f9377df049a5019051ba7d0f9c9c8fa34af5f5d
Santiago Molina Orozco <santi020k@gmail.com>
2023-10-22 02:29:57 -0600

    Add react prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,11 +19,11 @@ module.exports = {
         ecmaVersion: 2023
       },
       rules: {
-        // "astro/semi": "off",
+        "astro/semi": "off",
       },
     },
     {
-      files: ["*.mjs", "*.mts", "*.js", "*.jsx", "*.ts", "*.tsx"],
+      files: ["*.mjs", "*.mts", "*.js", "*.ts"],
       // Ts standard
       extends: "standard-with-typescript",
       env: {
@@ -38,11 +38,82 @@ module.exports = {
       },
     },
     {
-      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
-      extends: "standard-with-typescript",
+      files: ["*.jsx", "*.tsx"],
+      env: {
+        browser: true,
+        es2021: true
+      },
+      extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "standard-with-typescript"
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+      },
+      plugins: ["unused-imports", "react", "react-hooks", "@typescript-eslint", "jsx-a11y"],
       rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/button-has-type": "error",
+        "react/display-name": "error",
+        "react/no-children-prop": "error",
+        "react/no-danger-with-children": "error",
+        "react/jsx-no-bind": "error",
+        "arrow-body-style": "error",
+        "react/no-unstable-nested-components": "error",
+        "react/self-closing-comp": ["error", { "component": true, "html": true }],
+        "react/jsx-curly-brace-presence": ["error", { "props": "never", "children": "never" }],
+        "react/jsx-curly-newline": "error",
+        "react/destructuring-assignment": "error",
+        "react/jsx-pascal-case": "error",
+        "react/boolean-prop-naming": "error",
+        "react/hook-use-state": "error",
+        "react/jsx-boolean-value": "error",
+        "react/jsx-closing-tag-location": "error",
+        "react/jsx-closing-bracket-location": "error",
+        "react/jsx-wrap-multilines": "error",
+        "react/jsx-no-target-blank": "error",
+        "react/jsx-no-leaked-render": "error",
+        "react/jsx-handler-names": "error",
+        "react/jsx-fragments": "error",
+        "react/jsx-max-depth": ["error", { "max": 5 }],
+        "react/no-deprecated": "error",
+        "react/no-multi-comp": "error",
+        "react/no-unescaped-entities": "error",
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-empty-function": "error",
+        "@typescript-eslint/ban-types": "error",
+        "@typescript-eslint/no-var-requires": "error",
+        "@typescript-eslint/ban-ts-comment": "error",
+        "jsx-a11y/alt-text": "error",
+        "no-empty": "error",
+        "no-nested-ternary": "error",
+        "no-undef": "error",
+        "react/prop-types": 0,
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+          "error",
+          {
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            argsIgnorePattern: "^_"
+          }
+        ],
         "astro/semi": "off",
       },
+      settings: {
+        react: {
+          version: "detect"
+        }
+      }
     }
   ],
 };
