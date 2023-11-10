7891724e0dabe9b81ab1c4c4d57c4afb65fceae0
Santiago Molina Orozco <santi020k@gmail.com>
2023-11-09 22:23:56 -0600

    refactor(auth-session-utils.ts): move auth session utils and add new utilities

--- src/store/use-auth-store.ts
+++ src/store/use-auth-store.ts
@@ -6,7 +6,7 @@ import { toastError } from '@libs/toast-alerts/toast-alert'
 
 import { type UserData, UserDataSchema } from '@models/auth-model'
 
-import { parseAuthSession } from '@utils/parse-auth-session-utils'
+import { parseAuthSession } from '@utils/auth-session-utils'
 
 export interface UserAuthState {
   user: UserData
