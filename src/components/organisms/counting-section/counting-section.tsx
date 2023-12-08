41f69ff6948d544fe1fe36a9f288898b78bd8581
Santiago Molina Orozco <santi020k@gmail.com>
2023-12-07 18:51:07 -0600

    style(hero and counting-section): fix some styles errors, pixel perfect

--- src/components/organisms/counting-section/counting-section.tsx
+++ src/components/organisms/counting-section/counting-section.tsx
@@ -8,15 +8,13 @@ interface CountingSectionProps {
 }
 
 const CountingSection: FC<CountingSectionProps> = ({ t }) => (
-  <section
-    id="calc"
-    className="
-      container mx-auto flex min-h-screen flex-col flex-wrap items-center justify-center py-[140px] sm:py-[100px]
-    "
-  >
-    <FormCountingSection t={t} />
-    <ListCountingSection />
-  </section>
+  <>
+    <div id="calc" className='relative top-[-128px] sm:top-[-97px]' />
+    <section className="container mx-auto flex flex-col flex-wrap items-center justify-center py-[100px]">
+      <FormCountingSection t={t} />
+      <ListCountingSection />
+    </section>
+  </>
 )
 
 export default CountingSection
