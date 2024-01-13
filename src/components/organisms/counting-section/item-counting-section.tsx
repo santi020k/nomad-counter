d9e73042fda1edd68a30d4c624d1fa230491b1e8
Santiago Molina Orozco <santi020k@gmail.com>
2024-01-13 12:03:46 -0500

    feat(item-counting-section.tsx): add item counting days basic logic

--- src/components/organisms/counting-section/item-counting-section.tsx
+++ src/components/organisms/counting-section/item-counting-section.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable i18next/no-literal-string */
-import { type FC } from 'react'
+import { type FC, memo } from 'react'
 
 import { type ListCalculations } from '@libs/supabase/database.types'
 
@@ -11,29 +11,72 @@ interface ItemCountingSectionProps {
   index: number
 }
 
-const ItemCountingSection: FC<ItemCountingSectionProps> = ({ item, index }) => (
-  <tr>
-    {/* # */}
-    <th>{index}</th>
-    {/* Country */}
-    <td>{item.country}</td>
-    {/* Arrival */}
-    <td>{item.arrival}</td>
-    {/* Departure */}
-    <td>{item.departure}</td>
-    {/* Total Days */}
-    <td>All</td>
-    {/* Last Year Days */}
-    <td>181</td>
-    {/* 183 Days? */}
-    <td>No</td>
-    {/* Residency */}
-    <th>
-      <label>
-        <input type="checkbox" className="checkbox" disabled checked={item.isResidency} />
-      </label>
-    </th>
-  </tr>
-)
+const ItemCountingSection: FC<ItemCountingSectionProps> = ({ item, index }) => {
+  const isValidDate = (date: string): boolean => {
+    const newDate = new Date(date)
+    return !Number.isNaN(newDate.getTime())
+  }
 
-export default ItemCountingSection
+  const isValidDates = (arrival: string, departure: string): boolean => {
+    if (!isValidDate(arrival) || !isValidDate(departure)) return false
+    return true
+  }
+
+  const countTotalDays = (arrival: string, departure: string): number => {
+    if (!isValidDates(arrival, departure)) return 'All'
+    const arrivalDate = new Date(arrival)
+    const departureDate = new Date(departure)
+    const diffTime = Math.abs(departureDate.getTime() - arrivalDate.getTime())
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    return diffDays
+  }
+
+  const countLastYearDays = (arrival: string, departure: string): number => {
+  // TODO: This is not working properly, fix it!
+    if (!isValidDates(arrival, departure)) return 'All'
+    const arrivalDate = new Date(arrival)
+    const departureDate = new Date(departure)
+    const diffTime = Math.abs(departureDate.getTime() - arrivalDate.getTime())
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    return diffDays
+  }
+
+  const isMoreThan183Days = (arrival: string, departure: string): boolean => {
+    const totalDays = countTotalDays(arrival, departure)
+    if (!totalDays) return false
+    return totalDays > 183
+  }
+
+  const formatDate = (date: string): string => {
+    if (!isValidDate(date)) return '-------'
+    const newDate = new Date(date)
+    return newDate.toLocaleDateString()
+  }
+
+  return (
+    <tr>
+      {/* # */}
+      <th>{index}</th>
+      {/* Country */}
+      <td>{item.country}</td>
+      {/* Arrival */}
+      <td>{formatDate(item.arrival)}</td>
+      {/* Departure */}
+      <td>{formatDate(item.departure)}</td>
+      {/* Total Days */}
+      <td>{countTotalDays(item.arrival, item.departure) ?? 'All'}</td>
+      {/* Last Year Days */}
+      <td>{countLastYearDays(item.arrival, item.departure) ?? 181}</td>
+      {/* 183 Days? */}
+      <td>{isMoreThan183Days(item.arrival, item.departure) ? 'Si' : 'No'}</td>
+      {/* Residency */}
+      <th>
+        <label>
+          <input type="checkbox" className="checkbox" disabled checked={item.isResidency} />
+        </label>
+      </th>
+    </tr>
+  )
+}
+
+export default memo(ItemCountingSection)
