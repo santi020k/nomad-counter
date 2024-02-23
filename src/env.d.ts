2605ef10207e6d9314fb32322fd8ad2b09680f8e
Santiago Molina Orozco <santi020k@gmail.com>
2024-02-22 23:06:08 -0300

    fix(env.d.ts): fix eslint error env.d.ts error

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @stylistic/spaced-comment */
 /* eslint-disable @typescript-eslint/triple-slash-reference */
-// / <reference path="../.astro/types.d.ts" />
-// / <reference types="astro/client" />
-// / <reference types="vitest" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="vitest" />
 
 declare module 'preline/plugin'
